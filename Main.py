c02a45276b4f39fffd0bdc6dc6d9c0bc597d8c38
krystianres2 <91950213+krystianres2@users.noreply.github.com>
2023-12-02 18:17:45 +0100

    Input Field

--- Main.py
+++ Main.py
@@ -2,11 +2,16 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root, width=50, borderwidth=5)
+e.pack()
+e.insert(0, "Enter your name: ")
+
 def myClick():
-    myLabel = Label(root, text="Look! I clicked a Button!")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
 
-myButton = Button(root, text="Click me", command=myClick, fg="blue", bg="red")
+myButton = Button(root, text="Enter your name", command=myClick, fg="blue", bg="red")
 myButton.pack()
 
 
