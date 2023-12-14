795bdd7e93270698628a210233973a7749d0cda8
krystianres2 <91950213+krystianres2@users.noreply.github.com>
2023-12-14 15:19:38 +0100

    CheckBoxes

--- Main.py
+++ Main.py
@@ -4,21 +4,20 @@ from tkinter import messagebox
 from tkinter import filedialog
 
 root = Tk()
-root.title("Slider")
+root.title("Checkboxes")
 root.geometry("400x400")
 
-
-vertical = Scale(root, from_=0, to=200)
-vertical.pack()
-
-def slide():
-    my_label = Label(root, text=horizontal.get()).pack()
-    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()))
+def show():
+    myLabel = Label(root, text=var.get()).pack()
 
 
-horizontal = Scale(root, from_=0, to=400, orient=HORIZONTAL)
-horizontal.pack()
+var = StringVar()
 
-my_btn = Button(root, text="Click Me!", command=slide).pack()
+c = Checkbutton(root, text="Check this box, I dare you!", variable=var, onvalue="On", offvalue="Off")
+c.deselect()
+c.pack()
+
+
+myButton = Button(root, text="Show Selection", command=show).pack()
 
 mainloop()
